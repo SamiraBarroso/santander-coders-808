5a2cbdeff3d91edbf821b64f7beba1ae8f97987c
Bruno2090 <70537600+Bruno2090@users.noreply.github.com>
2021-11-09 21:41:11 -0300

    Add files via upload

---
+++ MODULO_04/atividades/atividade_05/assets/js/utils/keyboard.js
@@ -0,0 +1,28 @@
+export default function keyboardVirtual(){
+    const keyboard = document.getElementById("keyboard");
+    const bKeyboard = document.querySelector("#bKeyboard");
+    const inputWord = document.querySelector("#word-input");
+    const letter = document.querySelectorAll('[id="letter"]');
+
+    bKeyboard.addEventListener('click',function () {
+        if(keyboard.style.display == 'none'){
+            keyboard.style.display = "block";
+            inputWord.focus();
+        }else{
+            keyboard.style.display = "none";
+        }
+    });
+
+    window.addEventListener('DOMContentLoaded', (event) => {
+    letter.forEach(el => {
+        el.addEventListener('click',function () {
+            if(el.innerText.toLowerCase() != 'limpar'){
+                inputWord.value += el.innerHTML;
+                inputWord.focus();
+            }else{
+                inputWord.value = '';
+            }
+            });
+        });
+    });
+}
